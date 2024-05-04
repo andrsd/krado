5028012912c30a96edee15a266bb57a3c6b32f08
David Andrs <andrsd@gmail.com>
2024-05-04 07:06:51 -0600

    Adding test for geo::Shape

---
+++ test/src/geo/Shape_test.cpp
@@ -0,0 +1,43 @@
+#include "gmock/gmock.h"
+#include "krado/io/step_file.h"
+#include "krado/geo/shape.h"
+#include "krado/geo/vertex.h"
+#include "BRepLib_MakeVertex.hxx"
+#include "TopoDS.hxx"
+#include "TopoDS_Vertex.hxx"
+
+using namespace krado;
+namespace fs = std::filesystem;
+
+TEST(Geo_ShapeTest, clean)
+{
+    fs::path input_file = fs::path(KRADO_UNIT_TESTS_ROOT) / "assets" / "line.step";
+    STEPFile file(input_file.string());
+    auto shape = file.load();
+    shape.clean();
+    // TODO: actual check that clean happened
+}
+
+TEST(Geo_ShapeTest, heal)
+{
+    fs::path input_file = fs::path(KRADO_UNIT_TESTS_ROOT) / "assets" / "line.step";
+    STEPFile file(input_file.string());
+    auto shape = file.load();
+    shape.heal(1e-10);
+    // TODO: actual check that things were healed
+}
+
+TEST(Geo_ShapeTest, scale) {
+    gp_Pnt pt(1, 2, 3);
+    BRepLib_MakeVertex make_vtx(pt);
+    make_vtx.Build();
+    auto vtx = make_vtx.Vertex();
+
+    geo::Shape sh(vtx);
+    sh.scale(0.5);
+
+    geo::Vertex v(TopoDS::Vertex(sh));
+    EXPECT_DOUBLE_EQ(v.x(), 0.5);
+    EXPECT_DOUBLE_EQ(v.y(), 1.);
+    EXPECT_DOUBLE_EQ(v.z(), 1.5);
+}
