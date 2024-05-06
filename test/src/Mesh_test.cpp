8e5c1e1dabbaa70eee88af0259d5fb7986155347
David Andrs <andrsd@gmail.com>
2024-05-06 09:45:52 -0600

    Adding a test for creating a `Mesh` from a STEP file

---
+++ test/src/Mesh_test.cpp
@@ -0,0 +1,25 @@
+#include "gmock/gmock.h"
+#include "krado/step_file.h"
+#include "krado/model.h"
+#include "krado/mesh.h"
+#include <filesystem>
+
+using namespace krado;
+using namespace testing;
+namespace fs = std::filesystem;
+
+TEST(MeshTest, ctor)
+{
+    fs::path input_file = fs::path(KRADO_UNIT_TESTS_ROOT) / "assets" / "box.step";
+    STEPFile file(input_file.string());
+    auto shape = file.load();
+    Model model(shape);
+    Mesh mesh(model);
+    EXPECT_EQ(mesh.vertices().size(), 8);
+    EXPECT_EQ(mesh.curves().size(), 12);
+    EXPECT_EQ(mesh.surfaces().size(), 6);
+    EXPECT_EQ(mesh.volumes().size(), 1);
+
+    auto box = mesh.volume(27);
+    EXPECT_EQ(box.surfaces().size(), 6);
+}
