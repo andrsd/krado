41680bc750dbb8bd78cd0ef8c9bfcb39bcffc553
David Andrs <andrsd@gmail.com>
2024-05-09 10:26:22 -0600

    Adding tests for scheme 'equal'

---
+++ test/src/SchemeEqual_test.cpp
@@ -0,0 +1,48 @@
+#include "gmock/gmock.h"
+#include "krado/scheme_equal.h"
+#include "krado/geom_shape.h"
+#include "krado/geom_model.h"
+#include "krado/mesh.h"
+#include "builder.h"
+
+using namespace krado;
+
+TEST(SchemeEqualTest, line)
+{
+    auto shape = GeomShape(testing::build_line(Point(0, 0, 0), Point(2, 0, 0)));
+    GeomModel model(shape);
+    Mesh mesh(model);
+
+    auto & line = mesh.curve(3);
+    auto & mpars = line.meshing_parameters();
+    mpars.set<std::string>("scheme") = "equal";
+    mpars.set<int>("intervals") = 4;
+    mesh.mesh_curve(3);
+
+    ASSERT_EQ(line.all_vertices().size(), 5);
+    auto first_vtx = line.all_vertices().front();
+    auto last_vtx = line.all_vertices().back();
+    EXPECT_NE(first_vtx, last_vtx);
+
+    ASSERT_EQ(line.segments().size(), 4);
+}
+
+TEST(SchemeEqualTest, circle)
+{
+    auto shape = GeomShape(testing::build_circle(Point(0, 0, 0), 2));
+    GeomModel model(shape);
+    Mesh mesh(model);
+
+    auto & circ = mesh.curve(2);
+    auto & mpars = circ.meshing_parameters();
+    mpars.set<std::string>("scheme") = "equal";
+    mpars.set<int>("intervals") = 20;
+    mesh.mesh_curve(2);
+
+    ASSERT_EQ(circ.all_vertices().size(), 21);
+    auto first_vtx = circ.all_vertices().front();
+    auto last_vtx = circ.all_vertices().back();
+    EXPECT_EQ(first_vtx, last_vtx);
+
+    ASSERT_EQ(circ.segments().size(), 20);
+}
