f12be22433ccb6e56d3dcb818a5148be002b1635
David Andrs <andrsd@gmail.com>
2024-11-22 10:24:31 -0700

    Adding python wrapper for Vector ctor using Point

--- python/src/krado.cpp
+++ python/src/krado.cpp
@@ -63,6 +63,7 @@ PYBIND11_MODULE(krado, m)
 
     py::class_<Vector>(m, "Vector")
         .def(py::init<double, double, double>())
+        .def(py::init<const Point &>())
         .def_readwrite("x", &Vector::x)
         .def_readwrite("y", &Vector::y)
         .def_readwrite("z", &Vector::z)
