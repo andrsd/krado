b019a8f175175d4e5e2d70b0d8a4460c33b822d3
David Andrs <andrsd@gmail.com>
2025-04-14 08:08:45 -0600

    Adding missing `this`

--- lib/src/step_file.cpp
+++ lib/src/step_file.cpp
@@ -13,13 +13,13 @@ STEPFile::STEPFile(const std::string & file_name) : file_name_(file_name) {}
 GeomShape
 STEPFile::load() const
 {
-    TCollection_AsciiString fname(file_name_.c_str());
+    TCollection_AsciiString fname(this->file_name_.c_str());
 
     STEPControl_Reader reader;
     Interface_Static::SetIVal("read.step.ideas", 1);
     Interface_Static::SetIVal("read.step.nonmanifold", 1);
     if (reader.ReadFile(fname.ToCString()) != IFSelect_RetDone)
-        throw Exception("Could not read file '{}'", file_name_);
+        throw Exception("Could not read file '{}'", this->file_name_);
 
     reader.NbRootsForTransfer();
     reader.TransferRoots();
