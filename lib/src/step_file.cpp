4f3a12f656dc395e0895b10ec8330e040595b379
David Andrs <andrsd@gmail.com>
2025-04-17 07:00:48 -0600

    When reading from a STEP file, set material on `GeomShape`

--- lib/src/step_file.cpp
+++ lib/src/step_file.cpp
@@ -3,8 +3,14 @@
 
 #include "krado/step_file.h"
 #include "krado/exception.h"
-#include "Interface_Static.hxx"
-#include "STEPControl_Reader.hxx"
+#include "krado/geom_shape.h"
+#include "TDocStd_Document.hxx"
+#include "STEPCAFControl_Reader.hxx"
+#include "XCAFDoc.hxx"
+#include "XCAFDoc_Material.hxx"
+#include "XCAFDoc_DocumentTool.hxx"
+#include "XCAFDoc_ShapeTool.hxx"
+#include "TDataStd_TreeNode.hxx"
 
 namespace krado {
 
@@ -13,19 +19,37 @@ STEPFile::STEPFile(const std::string & file_name) : file_name_(file_name) {}
 std::vector<GeomShape>
 STEPFile::load() const
 {
-    TCollection_AsciiString fname(this->file_name_.c_str());
-
-    STEPControl_Reader reader;
-    Interface_Static::SetIVal("read.step.ideas", 1);
-    Interface_Static::SetIVal("read.step.nonmanifold", 1);
-    if (reader.ReadFile(fname.ToCString()) != IFSelect_RetDone)
-        throw Exception("Could not read file '{}'", this->file_name_);
-
-    reader.NbRootsForTransfer();
-    reader.TransferRoots();
     std::vector<GeomShape> shapes;
-    for (int idx = 1; idx <= reader.NbShapes(); ++idx)
-        shapes.push_back(GeomShape(-1, reader.Shape(idx)));
+
+    TCollection_AsciiString fname(this->file_name_.c_str());
+    STEPCAFControl_Reader reader;
+    reader.SetNameMode(Standard_True);
+    reader.SetMatMode(Standard_True);
+    Handle(TDocStd_Document) doc = new TDocStd_Document(TCollection_ExtendedString("formo-doc"));
+    if (!reader.Perform(fname, doc))
+        throw Exception("Failed to load STEP file");
+
+    auto shape_tool = XCAFDoc_DocumentTool::ShapeTool(doc->Main());
+    TDF_LabelSequence shape_labels;
+    shape_tool->GetShapes(shape_labels);
+
+    for (Standard_Integer i = 1; i <= shape_labels.Length(); ++i) {
+        auto shape_label = shape_labels.Value(i);
+        auto shape = XCAFDoc_ShapeTool::GetShape(shape_label);
+        GeomShape geom_shape(-1, shape);
+
+        Handle(TDataStd_TreeNode) node;
+        if (shape_label.FindAttribute(XCAFDoc::MaterialRefGUID(), node) && node->HasFather()) {
+            const TDF_Label material_label = node->Father()->Label();
+            Handle(XCAFDoc_Material) mat;
+            if (material_label.FindAttribute(XCAFDoc_Material::GetID(), mat)) {
+                geom_shape.set_material(mat->GetName()->ToCString(), mat->GetDensity());
+            }
+        }
+
+        shapes.push_back(geom_shape);
+    }
+
     return shapes;
 }
 
--- test/src/StepFile_test.cpp
+++ test/src/StepFile_test.cpp
@@ -26,5 +26,19 @@ TEST(STEPFileTest, load_non_existing)
             STEPFile file(input_file.string());
             auto shapes = file.load();
         },
-        HasSubstr("Could not read file"));
+        HasSubstr("Failed to load STEP file"));
+}
+
+TEST(STEPFileTest, load_materials)
+{
+    fs::path input_file = fs::path(KRADO_UNIT_TESTS_ROOT) / "assets" / "box-w-mat.step";
+
+    EXPECT_NO_THROW({
+        STEPFile file(input_file.string());
+        auto shapes = file.load();
+        ASSERT_EQ(shapes.size(), 1);
+        auto box = shapes[0];
+        EXPECT_EQ(box.material(), "steel");
+        EXPECT_EQ(box.density(), 8.);
+    });
 }
