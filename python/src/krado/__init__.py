0f660f0fbb49a61e6b56d4fe0b93cf7c44709c6a
David Andrs <andrsd@gmail.com>
2025-08-01 09:20:01 -0600

    Adding python classes into `__init__.py`

--- python/src/krado/__init__.py
+++ python/src/krado/__init__.py
@@ -6,5 +6,37 @@ from .krado import *
 __version__ = krado.__version__
 
 __all__ = [
+    "Axis1",
+    "Axis2",
+    "BoundingBox3D",
+    "CircularPattern",
+    "Element",
+    "ExodusIIFile",
+    "GeomCurve",
+    "GeomModel",
+    "GeomShape",
+    "GeomSurface",
+    "GeomVertex",
+    "GeomVolume",
+    "HexagonalPattern",
+    "LinearPattern",
+    "Mesh",
+    "MeshElement",
+    "MeshCurve"
+    "MeshCurveVertex",
+    "MeshSurface",
+    "MeshSurfaceVertex",
+    "MeshVertex",
+    "MeshVolume",
+    "Pattern",
+    "Point",
+    "Scheme"
     "STEPFile",
+    "Trsf",
+    "Vector",
+
+    "extrude",
+    "tetrahedralize",
+    "export_mesh",
+    "import_mesh"
 ]
