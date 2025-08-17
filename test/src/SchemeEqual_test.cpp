1f5048c01ccb863e7b4bcc8a63f28a9dc184f1f3
David Andrs <andrsd@gmail.com>
2025-08-17 16:35:06 -0600

    Fixing equal scheme to correctly mesh circular curves

--- test/src/SchemeEqual_test.cpp
+++ test/src/SchemeEqual_test.cpp
@@ -38,3 +38,34 @@ TEST(SchemeEqualTest, line)
 
     ASSERT_EQ(line->segments().size(), 5);
 }
+
+TEST(SchemeEqualTest, circle)
+{
+    auto circle = testing::build_circle(Point(0, 0, 0), 1);
+    GeomModel model(circle);
+
+    // clang-format off
+    model.curve(1)
+        ->set_scheme("equal")
+        .set("intervals", 8)
+    ;
+    // clang-format on
+    model.mesh_curve(1);
+
+    auto curv = model.curve(1);
+
+    auto SQRT2_2 = std::sqrt(2.) / 2.;
+
+    auto & all_vtxs = curv->all_vertices();
+    ASSERT_EQ(all_vtxs.size(), 8);
+    EXPECT_EQ(all_vtxs[0]->point(), Point(1., 0, 0));
+    EXPECT_EQ(all_vtxs[1]->point(), Point(SQRT2_2, SQRT2_2, 0));
+    EXPECT_EQ(all_vtxs[2]->point(), Point(0, 1., 0));
+    EXPECT_EQ(all_vtxs[3]->point(), Point(-SQRT2_2, SQRT2_2, 0));
+    EXPECT_EQ(all_vtxs[4]->point(), Point(-1., 0, 0));
+    EXPECT_EQ(all_vtxs[5]->point(), Point(-SQRT2_2, -SQRT2_2, 0));
+    EXPECT_EQ(all_vtxs[6]->point(), Point(0, -1., 0));
+    EXPECT_EQ(all_vtxs[7]->point(), Point(SQRT2_2, -SQRT2_2, 0));
+
+    EXPECT_EQ(curv->segments().size(), 8);
+}
