4e5eed1a88a3b66c697971863a849ba32387ae80
David Andrs <andrsd@gmail.com>
2025-12-06 13:35:25 -0700

    Updating `Box` to use `static create`

--- test/src/Box_test.cpp
+++ test/src/Box_test.cpp
@@ -5,6 +5,6 @@ using namespace krado;
 
 TEST(BoxTest, test)
 {
-    Box box(Point(0, 0, 0), Point(1, 2, 3));
+    auto box = Box::create(Point(0, 0, 0), Point(1, 2, 3));
     EXPECT_DOUBLE_EQ(box.volume(), 6.);
 }
