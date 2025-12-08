c046784d42a2f75a3de54b08b595dabbf918c9f6
David Andrs <andrsd@gmail.com>
2025-12-08 07:01:42 -0700

    Adding `bias` scheme

---
+++ lib/include/krado/scheme/bias.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2025 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "krado/ptr.h"
+#include "krado/scheme1d.h"
+
+namespace krado {
+
+class SchemeBias : public Scheme1D {
+public:
+    struct Options {
+        int intervals;
+        double factor;
+    };
+
+public:
+    SchemeBias(Options options);
+    void mesh_curve(Ptr<MeshCurve> surface) override;
+
+private:
+    Options opts_;
+};
+
+} // namespace krado
