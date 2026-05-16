a21177539c29afb1b5079244e4032dc5f9e89e6b
David Andrs <andrsd@gmail.com>
2026-05-16 06:49:24 -0600

    Adding `DocumentFile`
    - base class for STEP file,
    - and IGES file (in future)

---
+++ lib/include/krado/document_file.h
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2026 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "krado/geom_shape.h"
+#include <vector>
+#include <string>
+
+class TDocStd_Document;
+class XSControl_Reader;
+
+namespace krado {
+
+/// This class houses common functionality for reading/writing CAD files
+class DocumentFile {
+public:
+    /// Create a file object (no I/O done)
+    ///
+    /// @param file_name Name of the file to read
+    explicit DocumentFile(const std::string & file_name);
+
+    virtual ~DocumentFile() = default;
+
+    /// Get file name
+    const std::string file_name() const;
+
+protected:
+    TDocStd_Document * create_doc(const std::vector<GeomShape> & shapes);
+
+    std::string fname_;
+};
+
+} // namespace krado
---
+++ lib/src/document_file.cpp
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: 2026 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "krado/document_file.h"
+#include "krado/exception.h"
+#include "Standard_Handle.hxx"
+#include "TDocStd_Document.hxx"
+#include "XSControl_Reader.hxx"
+#include "XCAFDoc_DocumentTool.hxx"
+#include "XCAFDoc_ShapeTool.hxx"
+#include "XCAFDoc_ColorTool.hxx"
+#include "XCAFDoc_MaterialTool.hxx"
+#include "XCAFDoc_ColorType.hxx"
+#include "TDataStd_Name.hxx"
+#include "Quantity_Color.hxx"
+#include "Quantity_TypeOfColor.hxx"
+#include "TCollection_HAsciiString.hxx"
+
+namespace krado {
+
+DocumentFile::DocumentFile(const std::string & file_name) : fname_(file_name) {}
+
+const std::string
+DocumentFile::file_name() const
+{
+    return this->fname_;
+}
+
+TDocStd_Document *
+DocumentFile::create_doc(const std::vector<GeomShape> & shapes)
+{
+    auto doc = new TDocStd_Document(TCollection_ExtendedString("formo-doc"));
+    auto shape_tool = XCAFDoc_DocumentTool::ShapeTool(doc->Main());
+    auto color_tool = XCAFDoc_DocumentTool::ColorTool(doc->Main());
+    auto material_tool = XCAFDoc_DocumentTool::MaterialTool(doc->Main());
+    for (auto & shp : shapes) {
+        auto shape_label = shape_tool->AddShape(shp, false);
+        if (!shp.name().empty())
+            TDataStd_Name::Set(shape_label, TCollection_ExtendedString(shp.name().c_str()));
+
+        auto clr = shp.color();
+        Quantity_Color color(clr.redF(),
+                             clr.greenF(),
+                             clr.blueF(),
+                             Quantity_TypeOfColor::Quantity_TOC_RGB);
+        color_tool->SetColor(shape_label, color, XCAFDoc_ColorGen);
+
+        if (shp.has_material()) {
+            Handle(TCollection_HAsciiString) mat_name =
+                new TCollection_HAsciiString(shp.material().c_str());
+            Handle(TCollection_HAsciiString) mat_descr =
+                new TCollection_HAsciiString(shp.material_description().c_str());
+            Handle(TCollection_HAsciiString) density_name =
+                new TCollection_HAsciiString("Mass density");
+            Handle(TCollection_HAsciiString) density_value_type =
+                new TCollection_HAsciiString("g/cm^3");
+            auto mat_label = material_tool->AddMaterial(mat_name,
+                                                        mat_descr,
+                                                        shp.density(),
+                                                        density_name,
+                                                        density_value_type);
+            material_tool->SetMaterial(shape_label, mat_label);
+        }
+    }
+    return doc;
+}
+
+} // namespace krado
