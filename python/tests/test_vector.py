c754aa574237382a52f431f3dd779093ca22f3d6
David Andrs <andrsd@gmail.com>
2026-07-15 15:38:35 -0600

    Improving python tests for `Vector`

--- python/tests/test_vector.py
+++ python/tests/test_vector.py
@@ -4,6 +4,7 @@ import pytest
 
 def test_component():
     a = krado.Vector(1.0, 2.0, 3.0)
+    assert isinstance(a, krado.Vector)
     assert a.x == 1.0
     assert a.y == 2.0
     assert a.z == 3.0
@@ -13,12 +14,14 @@ def test_add():
     a = krado.Vector(1.0, 2.0, 3.0)
     b = krado.Vector(4.0, 5.0, 6.0)
     c = a + b
+    assert isinstance(c, krado.Vector)
     assert c.x == 5.0
     assert c.y == 7.0
     assert c.z == 9.0
 
     d = a
     d += c
+    assert isinstance(d, krado.Vector)
     assert d.x == 6.0
     assert d.y == 9.0
     assert d.z == 12.0
@@ -28,12 +31,14 @@ def test_subtract():
     a = krado.Vector(1.0, 2.0, 3.0)
     b = krado.Vector(6.0, 5.0, 4.0)
     c = a - b
+    assert isinstance(c, krado.Vector)
     assert c.x == -5.0
     assert c.y == -3.0
     assert c.z == -1.0
 
     d = a
     d -= c
+    assert isinstance(d, krado.Vector)
     assert d.x == 6.0
     assert d.y == 5.0
     assert d.z == 4.0
@@ -42,17 +47,20 @@ def test_subtract():
 def test_mult_scalar():
     a = krado.Vector(1, 2, 3)
     b = 2.0 * a
+    assert isinstance(b, krado.Vector)
     assert b.x == 2.0
     assert b.y == 4.0
     assert b.z == 6.0
 
     c = a * 3
+    assert isinstance(c, krado.Vector)
     assert c.x == 3.0
     assert c.y == 6.0
     assert c.z == 9.0
 
     d = a
     d *= 4
+    assert isinstance(d, krado.Vector)
     assert d.x == 4.0
     assert d.y == 8.0
     assert d.z == 12.0
@@ -61,7 +69,7 @@ def test_mult_scalar():
 def test_invert():
     a = krado.Vector(1, 2, 3)
     b = -a
-
+    assert isinstance(b, krado.Vector)
     assert b.x == -1.0
     assert b.y == -2.0
     assert b.z == -3.0
