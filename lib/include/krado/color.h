1fd2d9f1df308add72a395c33dcbe55d08547f96
David Andrs <andrsd@gmail.com>
2026-08-13 17:20:00 -0600

    clang-tidy: Color

--- lib/include/krado/color.h
+++ lib/include/krado/color.h
@@ -10,12 +10,12 @@ public:
     Color();
     Color(int r, int g, int b);
 
-    int red() const;
-    double redF() const;
-    int green() const;
-    double greenF() const;
-    int blue() const;
-    double blueF() const;
+    [[nodiscard]] int red() const;
+    [[nodiscard]] double redF() const;
+    [[nodiscard]] int green() const;
+    [[nodiscard]] double greenF() const;
+    [[nodiscard]] int blue() const;
+    [[nodiscard]] double blueF() const;
 
 private:
     int r_, g_, b_;
