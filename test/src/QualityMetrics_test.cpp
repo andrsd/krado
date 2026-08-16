fee3a79b30488719daf1e29ffffeb086690c28b7
David Andrs <andrsd@gmail.com>
2026-08-16 08:04:41 -0600

    Adding tests for skewness metric

--- test/src/QualityMetrics_test.cpp
+++ test/src/QualityMetrics_test.cpp
@@ -74,6 +74,74 @@ TEST(QualityMetricsTest, quad4_aspect_ratio)
     EXPECT_NEAR(ar, 2.0, 1e-12);
 }
 
+TEST(QualityMetricsTest, quad4_skewness_1)
+{
+    std::vector<Point> points = {
+        Point(0, 0, 0),
+        Point(1, 0, 0),
+        Point(1, 1, 0),
+        Point(0, 1, 0),
+    };
+    std::vector<Element> elements = { Element::Quad4({ 0, 1, 2, 3 }) };
+    Mesh mesh(points, elements);
+
+    double sk = qm::skewness<ElementType::QUAD4>(mesh.element(0), mesh);
+    EXPECT_NEAR(sk, 0., 1e-12);
+}
+
+TEST(QualityMetricsTest, quad4_skewness_2)
+{
+    std::vector<Point> points = {
+        Point(0, 0, 0),
+        Point(1, 0, 0),
+        Point(1.5, 1, 0),
+        Point(0.5, 1, 0),
+    };
+    std::vector<Element> elements = { Element::Quad4({ 0, 1, 2, 3 }) };
+    Mesh mesh(points, elements);
+
+    double sk = qm::skewness<ElementType::QUAD4>(mesh.element(0), mesh);
+    EXPECT_NEAR(sk, 0.2951672353, 1e-10);
+}
+
+TEST(QualityMetricsTest, hex8_skewness_1)
+{
+    std::vector<Point> points = {
+        Point(0, 0, 0),    Point(1, 0, 0),    Point(1.5, 1, 0),  Point(0.5, 1, 0),
+        Point(0.25, 0, 2), Point(1.25, 0, 2), Point(1.75, 1, 2), Point(0.75, 1, 2),
+    };
+    std::vector<Element> elements = { Element::Hex8({ 0, 1, 2, 3, 4, 5, 6, 7 }) };
+    Mesh mesh(points, elements);
+
+    double sk = qm::skewness<ElementType::HEX8>(mesh.element(0), mesh);
+    EXPECT_NEAR(sk, 0.293197777, 1e-9);
+}
+
+TEST(QualityMetricsTest, pri6_skewness_1)
+{
+    std::vector<Point> points = {
+        Point(0, 0, 0),    Point(1, 0, 0),    Point(1.5, 1, 0),
+        Point(0.25, 0, 2), Point(1.25, 0, 2), Point(1.75, 1, 2),
+    };
+    std::vector<Element> elements = { Element::Prism6({ 0, 1, 2, 3, 4, 5 }) };
+    Mesh mesh(points, elements);
+
+    double sk = qm::skewness<ElementType::PRISM6>(mesh.element(0), mesh);
+    EXPECT_NEAR(sk, 0.6698129955, 1e-10);
+}
+
+TEST(QualityMetricsTest, pyr5_skewness_1)
+{
+    std::vector<Point> points = {
+        Point(0, 0, 0), Point(1, 0, 0), Point(1.5, 1, 0), Point(0.5, 1, 0), Point(0.75, 0.5, 2),
+    };
+    std::vector<Element> elements = { Element::Pyramid5({ 0, 1, 2, 3, 4 }) };
+    Mesh mesh(points, elements);
+
+    double sk = qm::skewness<ElementType::PYRAMID5>(mesh.element(0), mesh);
+    EXPECT_NEAR(sk, 0.3160668323, 1e-10);
+}
+
 TEST(QualityMetricsTest, print_stats)
 {
     std::vector<Point> points = { Point(0, 0, 0), Point(1, 0, 0), Point(0.5, std::sqrt(3) / 2, 0) };
